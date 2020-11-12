f2e58797f7b931740b371d8f1bb33a9d3bb467c5
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-12 20:10:28 +0000

    Create stubs for website models based on the customer ERD

--- customer/website/models.py
+++ customer/website/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    pass
+
+class Account(models.Model):
+    pass
+
+
+
+class Purchase(models.Model):
+    pass
+
+class Concession(models.Model):
+    pass
+
+class Usage(models.Model):
+    pass
+
+
+
+class Ticket(models.Model):
+    pass
+
+class Transaction(models.Model):
+    pass
+
+class Discount(models.Model):
+    pass
+
+class MonetaryValue(models.Model):
+    pass
+
+
+
+class Location(models.Model):
+    pass
+
+class LatitudeLongitude(models.Model):
+    pass
+
+class UsageFromTo(models.Model):
+    pass
+
+class UsageReference(models.Model):
+    pass
+
+
+
+class Service(models.Model):
+    pass
+
+class TravelClass(models.Model):
+    pass
+
+class Vehicle(models.Model):
+    pass
+
+class RecordID(models.Model):
+    pass
