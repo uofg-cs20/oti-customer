44ec987df5ba04ce23e05ca3f9d9cb1904442774
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-12 20:11:03 +0000

    Create stub views and templates for the website

--- customer/website/views.py
+++ customer/website/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
-# Create your views here.
+def index(request):
+    return render(request, 'website/index.html')
+
+def register(request):
+    # TODO
+    return render(request, 'website/register.html')
+
+def customer_login(request):
+    # TODO
+    return render(request, 'website/login.html')
+
+def customer_logout(request):
+    logout(request)
+    return redirect(reverse('website:index'))
+
+def connect(request):
+    # TODO
+    return render(request, 'website/connect.html')
+
+def purchases(request):
+    # TODO
+    return render(request, 'website/purchases.html')
+
+def concessions(request):
+    # TODO
+    return render(request, 'website/concessions.html')
+
+def usage(request):
+    # TODO
+    return render(request, 'website/usage.html')
