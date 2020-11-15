f6c7493bd4b70a3740dc184659605d9918f7cf4d
Torrin <2378609m@student.gla.ac.uk>
2020-11-15 18:25:55 +0000

    Included required fields in models

--- customer/website/models.py
+++ customer/website/models.py
@@ -19,25 +19,25 @@ class Purchase(models.Model):
     booking_date_time = models.DateTimeField()
     transaction = models.OneToOneField("Transaction")
     account_balance = models.OneToOneField("MonetaryValue")
-    agent = models.CharField(max_length=100)
-    passenger_number = models.IntegerField()
-    passenger_type = models.CharField(max_length=100)
+    agent = models.CharField(max_length=100, null=True)
+    passenger_number = models.IntegerField(null=True)
+    passenger_type = models.CharField(max_length=100, null=True)
     vehicle = models.OneToOneField("Vehicle")
-    route = models.CharField(max_length=500)
+    route = models.CharField(max_length=500, null=True)
     travel_from_date_time = models.DateTimeField()
     travel_to_date_time = models.DateTimeField()
-    conditions = models.CharField(max_length=500)
+    conditions = models.CharField(max_length=500, null=True)
     concession = models.OneToOneField("Concession")
-    restrictions = models.CharField(max_length=500)
+    restrictions = models.CharField(max_length=500, null=True)
     ticket = models.OneToOneField("Ticket")
     # Although Purchase has a one-to-many relationship with Location, this is just
     # because Purchase has 2 location fields location_from and location_to.
     # These are still OneToOneFields
     location_from = models.OneToOneField("Location")
     location_to = models.OneToOneField("Location")
-    reserved_position = models.CharField(max_length=30)
+    reserved_position = models.CharField(max_length=30, null=True)
     # service_request has no relationship with the Service table, it's just a description
-    service_request = models.CharField(max_length=500)
+    service_request = models.CharField(max_length=500, null=True)
     customer_id = models.ForeignKey("Customer")
 
     def __str__(self):
@@ -46,18 +46,18 @@ class Purchase(models.Model):
 class Location(models.Model):
     lat_long = models.ForeignKey("LatitudeLongitude")
     NaPTAN = models.CharField(max_length=10)
-    other = models.CharField(max_length=30)
-    other_type = models.CharField(max_length=20)
-    accuracy = models.IntegerField()
+    other = models.CharField(max_length=30, null=True)
+    other_type = models.CharField(max_length=20, null=True)
+    accuracy = models.IntegerField(null=True)
 
     def __str__(self):
         return "Lat: " + str(self.lat_long.latitude) + ", Long: " + str(self.lat_long.longitude) + ", NaPTAN: " + self.NaPTAN
 
 class Vehicle(models.Model):
-    included = models.BooleanField()
+    included = models.BooleanField(default=True)
     reference = models.CharField(max_length=20)
     vehicle_type = models.CharField(max_length=20)
-    conditions = models.CharField(max_length=500)
+    conditions = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return "Type: " + self.vehicle_type + ", Reference: " + self.reference
