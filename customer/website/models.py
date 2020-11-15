da8ce73ec0d096cfe71110e26f0d7e7902a9de0e
Torrin <2378609m@student.gla.ac.uk>
2020-11-15 16:10:32 +0000

    Created Customer model

--- customer/website/models.py
+++ customer/website/models.py
@@ -13,7 +13,30 @@ class Account(models.Model):
 ########## Torrin #############
 
 class Purchase(models.Model):
-    pass
+    id = models.OneToOneField("RecordID") # This may change if our interpretation of RecordID was wrong
+    travel_class = models.OneToOneField("TravelClass")
+    booking_date_time = models.DateTimeField()
+    transaction = models.OneToOneField("Transaction")
+    account_balance = models.OneToOneField("MonetaryValue")
+    agent = models.CharField(max_length=100)
+    passenger_number = models.IntegerField()
+    passenger_type = models.CharField(max_length=100)
+    vehicle = models.OneToOneField("Vehicle")
+    route = models.CharField(max_length=500)
+    travel_from_date_time = models.DateTimeField()
+    travel_to_date_time = models.DateTimeField()
+    conditions = models.CharField(max_length=500)
+    concession = models.OneToOneField("Concession")
+    restrictions = models.CharField(max_length=500)
+    ticket = models.OneToOneField("Ticket")
+    # Although Purchase has a one-to-many relationship with Location, this is just
+    # because Purchase has 2 location fields location_from and location_to.
+    # These are still OneToOneFields
+    location_from = models.OneToOneField("Location")
+    location_to = models.OneToOneField("Location")
+    reserved_position = models.CharField(max_length=30)
+    service_request = models.CharField(max_length=500)
+    customer_id = models.ForeignKey("Customer")
 
 class Location(models.Model):
     pass
