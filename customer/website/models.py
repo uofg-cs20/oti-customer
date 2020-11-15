f6032ab38289e4e6bc7538faddcb1678d102f698
Torrin <2378609m@student.gla.ac.uk>
2020-11-15 17:13:31 +0000

    Created Location, Vehicle & LatitudeLongitude models

--- customer/website/models.py
+++ customer/website/models.py
@@ -13,7 +13,8 @@ class Account(models.Model):
 ########## Torrin #############
 
 class Purchase(models.Model):
-    id = models.OneToOneField("RecordID") # This may change if our interpretation of RecordID was wrong
+    # id may change if our interpretation of RecordID was wrong
+    id = models.OneToOneField("RecordID", primary_key=True)
     travel_class = models.OneToOneField("TravelClass")
     booking_date_time = models.DateTimeField()
     transaction = models.OneToOneField("Transaction")
@@ -35,17 +36,26 @@ class Purchase(models.Model):
     location_from = models.OneToOneField("Location")
     location_to = models.OneToOneField("Location")
     reserved_position = models.CharField(max_length=30)
+    # service_request has no relationship with the Service table, it's just a description
     service_request = models.CharField(max_length=500)
     customer_id = models.ForeignKey("Customer")
 
 class Location(models.Model):
-    pass
+    lat_long = models.ForeignKey("LatitudeLongitude")
+    NaPTAN = models.CharField(max_length=10)
+    other = models.CharField(max_length=30)
+    other_type = models.CharFirled(max_length=20)
+    accuracy = models.IntegerField()
 
 class Vehicle(models.Model):
-    pass
+    included = models.BooleanField()
+    reference = models.CharField(max_length=20)
+    vehicle_type = models.CharField(max_length=20)
+    conditions = models.CharField(max_length=500)
 
 class LatitudeLongitude(models.Model):
-    pass
+    latitude = models.DecimalField(max_digits=6, decimal_places=4)
+    longitude = models.DecimalField(max_digits=7, decimal_places=4)
 
 
 ############ Tomas #############
