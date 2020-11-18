cac6458afcfa809d6f956d757783ee342fbc1ab3
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-18 13:56:36 +0000

    Change website homepage to the login page

--- customer/website/urls.py
+++ customer/website/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('login/', views.customer_login, name='login'),
+    #path('', views.index, name='index'),
+    #path('register/', views.register, name='register'),
+    path('', views.customer_login, name='login'),
     path('logout/', views.customer_logout, name='logout'),
     path('connect/', views.connect, name='connect'),
     path('purchases/', views.purchases, name='purchases'),
