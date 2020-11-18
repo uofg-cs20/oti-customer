00c326f4ef078bc14c8fcd9e41fa6d64b4807c86
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-18 20:15:44 +0000

    Add another customer & their data to the population script

--- customer/populate.py
+++ customer/populate.py
@@ -5,6 +5,7 @@ import django
 django.setup()
 
 import decimal 
+import datetime
 
 from django.contrib.auth.models import User
 from website.models import *
@@ -12,7 +13,7 @@ from django.utils.timezone import make_aware
 
 def populate():
 
-    # delete data f the database is already populated
+    # delete data if the database is already populated
     if User.objects.filter(username='dev').exists():
         User.objects.all().delete()
     if Customer.objects.all():
@@ -50,60 +51,89 @@ def populate():
     if Mode.objects.all():
         Mode.objects.all().delete()    
 
-
-
-    d = decimal.Decimal(1.5312) 
-    d2 = decimal.Decimal(2.3245)
-    d3 = decimal.Decimal(3.5312) 
-    d4 = decimal.Decimal(4.3245)
-    d5 = decimal.Decimal(5.5312) 
-    d6 = decimal.Decimal(6.3245)
-    d7 = decimal.Decimal(7.5312) 
-    d8 = decimal.Decimal(8.3245)
-
     # superuser account - use this to log into the django admin page
     dev = User.objects.create_user(username='dev', password='1234', is_superuser=True, is_staff=True, email="dev@project.com", first_name='dev')
 
-    # create a customer
-    user1 = User.objects.create_user(username='tomas', password='1234', email='tomas@scotrail.co.uk.', first_name='Tomas Mikus')
+    # general stuff
+    train, created = Mode.objects.get_or_create(id="train", short_desc="Train")
+    first_class, created = TravelClass.objects.get_or_create(travel_class="First Class")
+    vehicle_type, created = Vehicle.objects.get_or_create(reference="train 3001", vehicle_type="train")
+
+    # create customer 1
+    user1 = User.objects.create_user(username='tomas', password='1234', email='tomas@scotrail.co.uk.', first_name='Tomas')
     customer1, created = Customer.objects.get_or_create(user=user1)
     account1, created = Account.objects.get_or_create(customer=customer1, operator_id="Scotrail")
 
-    # add a concession purchase to a customer account
-    record, created = RecordID.objects.get_or_create(id="00000001")
-    price, created = MonetaryValue.objects.get_or_create(amount="30.00", currency="GBP")
-    transaction, created = Transaction.objects.get_or_create(payment_type="Visa", payment_method="Debit", price=price)
-    discount, created = Discount.objects.get_or_create(discount_type="Young person", discount_value="0.20", discount_description="16-25 Railcard")
-    concession, created = Concession.objects.get_or_create(id=record, name="16-25 Railcard", price="30.00", discount=discount, transaction=transaction, valid_from_date_time="2020-01-01", valid_to_date_time="2022-01-01", conditions="Below 25", customer=customer1)
+    # add a concession purchase to the customer1 account
+    record1, created = RecordID.objects.get_or_create(id="00000001")
+    price1, created = MonetaryValue.objects.get_or_create(amount="30.00", currency="GBP")
+    transaction1, created = Transaction.objects.get_or_create(payment_type="Visa", payment_method="Debit", price=price1)
+    discount1, created = Discount.objects.get_or_create(discount_type="Young person", discount_value="0.20", discount_description="16-25 Railcard")
+    concession1, created = Concession.objects.get_or_create(id=record1, name="16-25 Railcard", price="30.00", discount=discount1, transaction=transaction1, valid_from_date_time=django.utils.timezone.now(), valid_to_date_time=django.utils.timezone.now()+datetime.timedelta(days=728), conditions="Below 25", customer=customer1)
 
+    # add a purchase to the customer1 account
     record2, created = RecordID.objects.get_or_create(id="00000002")
-    train, created = Mode.objects.get_or_create(id="train", short_desc="Train")
-    first_class, created = TravelClass.objects.get_or_create(travel_class="First Class")
     price2, created = MonetaryValue.objects.get_or_create(amount="10.00", currency="GBP")
     transaction2, created = Transaction.objects.get_or_create(payment_type="Visa", payment_method="Debit", price=price2)
-    account_balance1, created = MonetaryValue.objects.get_or_create(amount="100.00", currency="GBP")
-    vehicle_type, created = Vehicle.objects.get_or_create(reference="train 3001", vehicle_type="train")
-    ticket1, created = Ticket.objects.get_or_create(reference="GLA-STL 102413134", number_usages="0", reference_type="Idk", medium="Idk 2")
-    coordinates_from, created = LatitudeLongitude.objects.get_or_create(latitude=d, longitude=d2)
-    location_from, created = Location.objects.get_or_create(lat_long=coordinates_from, NaPTAN="idk")
-    coordinates_to, created = LatitudeLongitude.objects.get_or_create(latitude=d3, longitude=d4)
-    location_to, created = Location.objects.get_or_create(lat_long=coordinates_to, NaPTAN="idk")
-    purchase1, created = Purchase.objects.get_or_create(id=record2, mode=train, travel_class=first_class, booking_date_time=django.utils.timezone.now(), transaction=transaction2, account_balance=account_balance1, vehicle=vehicle_type, travel_from_date_time=django.utils.timezone.now(), travel_to_date_time=django.utils.timezone.now(), concession=concession, ticket=ticket1, location_from=location_from, location_to=location_to, customer=customer1)
-
-
-
-    record3, created = RecordID.objects.get_or_create(id="00000003")
-    usage_reference1, created = UsageReference.objects.get_or_create(reference="001", reference_type="usage type")
-
-    coordinates_to2, created = LatitudeLongitude.objects.get_or_create(latitude=d5, longitude=d6)
-    location_to2, created = Location.objects.get_or_create(lat_long=coordinates_to2, NaPTAN="idk")
-    travel_to2, created = UsageFromTo.objects.get_or_create(location=location_to2, date_time=django.utils.timezone.now(), reference="reference usage")
-
-    coordinates_from2, created = LatitudeLongitude.objects.get_or_create(latitude=d7, longitude=d8)
+    account_balance2, created = MonetaryValue.objects.get_or_create(amount="100.00", currency="GBP")
+    ticket2, created = Ticket.objects.get_or_create(reference="GLA-STL 102413134", number_usages="0", reference_type="Idk", medium="Idk 2")
+    coordinates_from2, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(1.5312), longitude=decimal.Decimal(2.5312))
     location_from2, created = Location.objects.get_or_create(lat_long=coordinates_from2, NaPTAN="idk")
-    travel_from2, created = UsageFromTo.objects.get_or_create(location=location_from2, date_time=django.utils.timezone.now(), reference="reference usage")
+    coordinates_to2, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(3.5312), longitude=decimal.Decimal(4.5312))
+    location_to2, created = Location.objects.get_or_create(lat_long=coordinates_to2, NaPTAN="idk")
+    purchase2, created = Purchase.objects.get_or_create(id=record2, mode=train, travel_class=first_class, booking_date_time=django.utils.timezone.now(), transaction=transaction2, account_balance=account_balance2, vehicle=vehicle_type, travel_from_date_time=django.utils.timezone.now(), travel_to_date_time=django.utils.timezone.now()+datetime.timedelta(days=30), concession=concession1, ticket=ticket2, location_from=location_from2, location_to=location_to2, customer=customer1)
 
-    usage, created = Usage.objects.get_or_create(id=record3, mode=train, reference=usage_reference1, travel_class=first_class, travel_from=travel_from2, travel_to=travel_to2, purchase_id=purchase1, ticket_reference=ticket1, price=price2, customer=customer1)
+    # add a usage to the customer1 account
+    record3, created = RecordID.objects.get_or_create(id="00000003")
+    usage_reference3, created = UsageReference.objects.get_or_create(reference="001", reference_type="usage type")
+
+    coordinates_to3, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(5.5312), longitude=decimal.Decimal(6.5312))
+    location_to3, created = Location.objects.get_or_create(lat_long=coordinates_to3, NaPTAN="idk")
+    travel_to3, created = UsageFromTo.objects.get_or_create(location=location_to3, date_time=django.utils.timezone.now(), reference="reference usage")
+
+    coordinates_from3, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(7.5312), longitude=decimal.Decimal(8.5312))
+    location_from3, created = Location.objects.get_or_create(lat_long=coordinates_from3, NaPTAN="idk")
+    travel_from3, created = UsageFromTo.objects.get_or_create(location=location_from3, date_time=django.utils.timezone.now()-datetime.timedelta(days=10), reference="reference usage")
+
+    usage3, created = Usage.objects.get_or_create(id=record3, mode=train, reference=usage_reference3, travel_class=first_class, travel_from=travel_from3, travel_to=travel_to3, purchase_id=purchase2, ticket_reference=ticket2, price=price2, customer=customer1)
+
+    # create customer 2
+    user2 = User.objects.create_user(username='torrin', password='1234', email='torrin@scotrail.co.uk.', first_name='Torrin')
+    customer2, created = Customer.objects.get_or_create(user=user2)
+    account2, created = Account.objects.get_or_create(customer=customer2, operator_id="Scotrail")
+
+    # add a concession purchase to the customer2 account
+    record6, created = RecordID.objects.get_or_create(id="00000006")
+    price6, created = MonetaryValue.objects.get_or_create(amount="10.00", currency="GBP")
+    transaction6, created = Transaction.objects.get_or_create(payment_type="Visa", payment_method="Debit", price=price6)
+    discount6, created = Discount.objects.get_or_create(discount_type="Senior Traveller", discount_value="0.50", discount_description="Over 60 yrs")
+    concession6, created = Concession.objects.get_or_create(id=record6, name="Senior Traveller", price="10.00", discount=discount6, transaction=transaction6, valid_from_date_time=django.utils.timezone.now(), valid_to_date_time=django.utils.timezone.now()+datetime.timedelta(days=364), conditions="Over 60", customer=customer2)
+
+    # add a purchase to the customer2 account
+    record4, created = RecordID.objects.get_or_create(id="00000004")
+    price4, created = MonetaryValue.objects.get_or_create(amount="15.00", currency="GBP")
+    transaction4, created = Transaction.objects.get_or_create(payment_type="Visa", payment_method="Debit", price=price4)
+    account_balance4, created = MonetaryValue.objects.get_or_create(amount="50.00", currency="GBP")
+    ticket4, created = Ticket.objects.get_or_create(reference="GLA-EDB 102413134", number_usages="0", reference_type="Idk", medium="Idk 2")
+    coordinates_from4, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(1.5312), longitude=decimal.Decimal(2.5312))
+    location_from4, created = Location.objects.get_or_create(lat_long=coordinates_from4, NaPTAN="idk")
+    coordinates_to4, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(3.5312), longitude=decimal.Decimal(4.5312))
+    location_to4, created = Location.objects.get_or_create(lat_long=coordinates_to4, NaPTAN="idk")
+    purchase4, created = Purchase.objects.get_or_create(id=record4, mode=train, travel_class=first_class, booking_date_time=django.utils.timezone.now(), transaction=transaction4, account_balance=account_balance4, vehicle=vehicle_type, travel_from_date_time=django.utils.timezone.now(), travel_to_date_time=django.utils.timezone.now()+datetime.timedelta(days=30), concession=concession6, ticket=ticket4, location_from=location_from4, location_to=location_to4, customer=customer2)
+
+    # add a usage to the customer2 account
+    record5, created = RecordID.objects.get_or_create(id="00000005")
+    usage_reference5, created = UsageReference.objects.get_or_create(reference="002", reference_type="usage type")
+
+    coordinates_to5, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(5.5312), longitude=decimal.Decimal(6.5312))
+    location_to5, created = Location.objects.get_or_create(lat_long=coordinates_to5, NaPTAN="idk")
+    travel_to5, created = UsageFromTo.objects.get_or_create(location=location_to5, date_time=django.utils.timezone.now(), reference="reference usage")
+
+    coordinates_from5, created = LatitudeLongitude.objects.get_or_create(latitude=decimal.Decimal(7.5312), longitude=decimal.Decimal(8.5312))
+    location_from5, created = Location.objects.get_or_create(lat_long=coordinates_from5, NaPTAN="idk")
+    travel_from5, created = UsageFromTo.objects.get_or_create(location=location_from5, date_time=django.utils.timezone.now()-datetime.timedelta(days=10), reference="reference usage")
+
+    usage5, created = Usage.objects.get_or_create(id=record5, mode=train, reference=usage_reference5, travel_class=first_class, travel_from=travel_from5, travel_to=travel_to5, purchase_id=purchase4, ticket_reference=ticket4, price=price4, customer=customer2)
 
 if __name__ == '__main__':
     print('Starting population script...', end="")
