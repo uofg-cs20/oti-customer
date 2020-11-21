31305400d613f4a802698b446d211320922a2829
Jamie McKay <2392436m@student.gla.ac.uk>
2020-11-21 12:14:33 +0000

    Added login and base.html

--- customer/website/views.py
+++ customer/website/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .models import Mode, Purchase, Concession, Usage
 from .helper_functions import formatDate, getPurchases
 from datetime import date
+
+from .forms import LoginForm
+
 
 def index(request):
     return render(request, 'website/index.html')
@@ -13,8 +18,19 @@ def register(request):
     return render(request, 'website/register.html')
 
 def customer_login(request):
-    # TODO
-    return render(request, 'website/login.html')
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            loginName = form.cleaned_data['username']
+            loginPass = form.cleaned_data['password']
+
+            user = authenticate(username = loginName, password = loginPass)
+            login(request, user)
+
+            return redirect(reverse('website:purchases'))
+
+    return render(request, 'website/login.html', {"form": form})
 
 def customer_logout(request):
     logout(request)
@@ -28,7 +44,7 @@ def purchases(request):
     # Initialise a context dictionary to store the Purchases and available
     # modes of transport
     context = {"purchases":[], "modes":Mode.objects.all()}
-    
+
     if request.method == "POST":
         # Check if filters have been applied, store these in the
         # context dictionary to process
@@ -43,13 +59,13 @@ def purchases(request):
             context.update({"enddate":enddate})
         if mode and mode != "None":
             context.update({"mode":mode})
-    
+
     # Retrieve a list of Purchases filtered by the given fields in
     # the context dictionary, and store these in the context dictionary
     purchases = getPurchases(request.user, context)
     for p in purchases:
         context["purchases"].append(p)
-        
+
     return render(request, 'website/purchases.html', context)
 
 def concessions(request):
