3348d7264b7297b4973e767cf5e1937c49d927e1
Paul Traynor <2205382t@student.gla.ac.uk>
2021-01-08 16:00:59 +0000

    created serializers for Mode, MonetaryValue, Discount, Transaction and Customer

--- customer/website/serializers.py
+++ customer/website/serializers.py
@@ -1,4 +1,4 @@
-from .models import Purchase, Concession, Usage
+from .models import Purchase, Concession, Usage, Mode, MonetaryValue, Discount, Transaction, Customer
 from rest_framework import serializers
 
 """
@@ -11,7 +11,40 @@ class should overwrite fields which are foreign keys
 with an instance of that respective serializer.
 """
 
+class ModeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mode
+        fields = '__all__'
+
+class MonetaryValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MonetaryValue
+        fields = '__all__'
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    price = MonetaryValueSerializer()
+
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
 class ConcessionSerializer(serializers.ModelSerializer):
+    mode = ModeSerializer()
+    price = MonetaryValueSerializer()
+    discount = DiscountSerializer()
+    transaction = TransactionSerializer()
+    customer = CustomerSerializer()
+
     class Meta:
         model = Concession
         fields = '__all__'
