5dfa08d33b6000f396719bc6b1d65c6b66369ec3
Kameron <2386181e@student.gla.ac.uk>
2021-01-10 03:23:24 +0000

    Created populate_large.py . Currently creates a lot of the data randomly, should be good enough for testing. I'm v tired so if you have any questions just ask me and I'll try explain

--- customer/website/models.py
+++ customer/website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 # Please refer to https://app.swaggerhub.com/apis/open-transport/customer-account/1.0.1#/
 # for details on these models
