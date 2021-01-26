883968861258ffce50704cf0b9b9dcd1893f6956
Torrin <2378609m@student.gla.ac.uk>
2021-01-26 15:03:00 +0000

    Formatted all field names to have hyphens instead of underscores

--- customer/website/serializers.py
+++ customer/website/serializers.py
@@ -3,10 +3,26 @@ from .models import TravelClass, UsageFromTo, Location, LatitudeLongitude, Vehic
 from rest_framework import serializers
 
 
+# Given a list of field names with a given delimiter, returns a list of
+# field names with the specified character as a delimiter, along with a
+# dictionary of source fields
+def formatFields(fields, current_delimiter='_', new_delimiter='-'):
+    formatted_fields = []
+    source_fields = {}
+    for f in fields:
+        formatted_str = f
+        if current_delimiter in f:
+            formatted_str = f.replace(current_delimiter, new_delimiter)
+            source_fields[formatted_str] = {'source': f}
+        formatted_fields.append(formatted_str)
+    return formatted_fields, source_fields
+
+
 class ModeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mode
-        fields = '__all__'
+        fields = ['id', 'short_desc', 'long_desc']
+        fields, extra_kwargs = formatFields(fields)
         
 class OperatorSerializer(serializers.ModelSerializer):
     modes = ModeSerializer(many=True)
@@ -14,16 +30,19 @@ class OperatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Operator
         fields = ['id', 'name', 'modes', 'homepage', 'api_url', 'default_language', 'phone', 'email']
+        fields, extra_kwargs = formatFields(fields)
 
 class MonetaryValueSerializer(serializers.ModelSerializer):
     class Meta:
         model = MonetaryValue
         fields = ['amount', 'currency']
+        fields, extra_kwargs = formatFields(fields)
 
 class DiscountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discount
         fields = ['discount_type', 'discount_value', 'discount_description']
+        fields, extra_kwargs = formatFields(fields)
 
 class TransactionSerializer(serializers.ModelSerializer):
     price = MonetaryValueSerializer()
@@ -31,18 +50,25 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = ['date_time', 'reference', 'payment_type', 'payment_method', 'price']
+        fields, extra_kwargs = formatFields(fields)
 
 class LatitudeLongitudeSerializer(serializers.ModelSerializer):
     class Meta:
         model = LatitudeLongitude
         fields = ['latitude', 'longitude']
+        fields, extra_kwargs = formatFields(fields)
 
 class LocationSerializer(serializers.ModelSerializer):
-    lat_long = LatitudeLongitudeSerializer()
+    #lat_long = LatitudeLongitudeSerializer()
+
+    def __init__(self, *args, **kwargs):
+        self.fields["lat-long"] = LatitudeLongitudeSerializer(source="lat_long")
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = Location
         fields = ['lat_long', 'NaPTAN', 'other', 'other_type', 'accuracy']
+        fields, extra_kwargs = formatFields(fields)
 
 class UsageFromToSerializer(serializers.ModelSerializer):
     location = LocationSerializer()
@@ -50,21 +76,25 @@ class UsageFromToSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsageFromTo
         fields = ['location', 'date_time', 'reference']
+        fields, extra_kwargs = formatFields(fields)
 
 class UsageReferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsageReference
         fields = ['reference', 'reference_type']
+        fields, extra_kwargs = formatFields(fields)
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
         fields = ['included', 'reference', 'vehicle_type', 'conditions']
+        fields, extra_kwargs = formatFields(fields)
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ['number_usages', 'reference', 'reference_type', 'medium']
+        fields, extra_kwargs = formatFields(fields)
         
 class ServiceSerializer(serializers.ModelSerializer):
     price = MonetaryValueSerializer()
@@ -72,6 +102,7 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ['service_type', 'unit', 'amount', 'price']
+        fields, extra_kwargs = formatFields(fields)
 
 class ConcessionSerializer(serializers.ModelSerializer):
     mode = ModeSerializer()
@@ -83,33 +114,48 @@ class ConcessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Concession
         fields = ['id', 'mode', 'operator', 'name', 'price', 'discount', 'transaction', 'valid_from_date_time', 'valid_to_date_time', 'conditions']
+        fields, extra_kwargs = formatFields(fields)
 
 class PurchaseSerializer(serializers.ModelSerializer):
     mode = ModeSerializer()
     operator = OperatorSerializer()
     concession = serializers.StringRelatedField()
-    account_balance = MonetaryValueSerializer()
+    #account_balance = MonetaryValueSerializer()
     vehicle = VehicleSerializer()
-    location_from = LocationSerializer()
-    location_to = LocationSerializer()
+    #location_from = LocationSerializer()
+    #location_to = LocationSerializer()
     transaction = TransactionSerializer()
     ticket = TicketSerializer()
+    
+    def __init__(self, *args, **kwargs):
+        self.fields["account-balance"] = MonetaryValueSerializer(source="account_balance")
+        self.fields["location-from"] = LocationSerializer(source="location_from")
+        self.fields["location-to"] = LocationSerializer(source="location_to")
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = Purchase
         fields = ['id', 'mode', 'operator', 'travel_class', 'booking_date_time', 'transaction', 'account_balance', 'agent', 'passenger_number', 'passenger_type', 'vehicle', 'route', 'travel_from_date_time', 'travel_to_date_time', 'conditions', 'concession', 'restrictions', 'ticket', 'location_from', 'location_to', 'reserved_position', 'service_request']
+        fields, extra_kwargs = formatFields(fields)
 
 class UsageSerializer(serializers.ModelSerializer):
     mode = ModeSerializer()
     operator = OperatorSerializer()
     reference = UsageReferenceSerializer()
-    travel_from = UsageFromToSerializer()
-    travel_to = UsageFromToSerializer()
+    #travel_from = UsageFromToSerializer()
+    #travel_to = UsageFromToSerializer()
     ticket = TicketSerializer()
     price = MonetaryValueSerializer()
     services = ServiceSerializer(many=True)
     
+    def __init__(self, *args, **kwargs):
+        self.fields["travel-from"] = UsageFromToSerializer(source="travel_from")
+        self.fields["travel-to"] = UsageFromToSerializer(source="travel_to")
+        super().__init__(*args, **kwargs)
+    
     class Meta:
         model = Usage
         fields = ['id', 'mode', 'operator', 'reference', 'travel_class', 'travel_from', 'travel_to', 'purchase_id', 'route_via_avoid', 'ticket', 'pre_paid', 'price', 'services']
+        fields, extra_kwargs = formatFields(fields)
+
 
